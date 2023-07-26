5193154d84948bb32ee4e69ee14112d5d8a48b6c
nagiriko <r.nagihashi@gmail.com>
2023-07-26 16:49:37 +0900

    メッセージの表示

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_163333) do
     t.string "content"
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
